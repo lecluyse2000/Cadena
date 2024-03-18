8f707daa4ad242c35c7e0bcb2b57649ee2b86c38
lecluyse2000 <lecluyse20@gmail.com>
2024-03-18 01:27:56 -0500

    Switched around public and private in hashtable

--- src/hashtable.h
+++ src/hashtable.h
@@ -22,22 +22,22 @@ struct login
 
 class HashTable
 {
-   private:
-      int m_numberBuckets;
-      std::size_t m_vectorSize;
-      std::vector<std::vector<login>> m_table;
-        
    public: 
       HashTable();
       void resize(const std::size_t size);
       bool isEmpty() const noexcept;
       int getSize() const noexcept;
-      const std::size_t hashFunction(const std::string_view key) const noexcept;
       void insertNode(const std::string& key, const std::string& value);
       void removeNode(const std::string_view key);
       const std::string searchTable(const std::string_view key) const noexcept;
       void printTable() const noexcept;
       void changePassword(const std::string_view key, const std::string& newPassword);
+   
+   private:
+      int m_numberBuckets;
+      std::size_t m_vectorSize;
+      std::vector<std::vector<login>> m_table;
+      const std::size_t hashFunction(const std::string_view key) const noexcept;
 };
 
 #endif
